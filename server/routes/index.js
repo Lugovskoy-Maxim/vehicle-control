77c254db16312dd14e39bfe01afe148315ca220c
Максим Луговской <lugovskou.myu@yandex.ru>
2023-02-14 23:56:18 +0500

    docx: add coments

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,16 +8,18 @@ const { ERROR_404_PAGE_MESSAGE } = require('../utils/constants')
 
 router.use('/signin', validateAuth, login);
 router.use('/signup', validateRegister, registrations);
-
+// Защищаю все последующие роуте авторизациейы
 router.use(auth);
 // При работе с cookie придется отправлять запрос для удаления http: only
 router.get('/signout', (req, res) => {
   res.clearCookie('jwtToken').send({ message: 'Выход' });
 });
 
-router.use(user);
-router.use(vehicle);
-router.use(pass);
+router.use(user);  // Роутер пользователей
+router.use(vehicle);  // Роутер техники
+router.use(pass);  // Роутер пропусков(объектов)
+// router.use(news); // Роутер объявлений
+// router.use(registerContracts); // Роутер договоров
 
 router.use('/*', () => { throw new NotFoundError(ERROR_404_PAGE_MESSAGE); }); // при переходе на несуществующий адресc
 
