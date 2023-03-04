ebed12589901d36024d5ec3e0d4bd0d58598a99f
Максим Луговской <lugovskou.myu@yandex.ru>
2023-03-04 18:33:17 +0500

    add: fonts normalize header icons

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -2,14 +2,23 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        <Route path='/' element={
+            <>
+              <Header />
+              <Main />
+              <Footer />
+            </>
+          }
+        />
+
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import styles from './Header.module.css'
+import { Link } from "react-router-dom";
+import styles from './Header.module.css';
+import avatar from '../../images/avatar.JPG';
 
 function Header(){
 
   return(
     <header className={styles.header}>
-
+      <Link className={styles.link} to='/'>
+        <p className={styles.name}>Vehicle</p>
+        <p className={styles.slogan}>control</p>
+      </Link>
+      <div className={styles.profile}>
+        <ul className={styles.iconBar}>
+          <li className={styles.notification}>
+            иконка 1
+          </li>
+          <li className={styles.notification}>
+            иконка 2
+          </li>
+        </ul>
+        <div className={styles.avatar}>
+          <img className={styles.avatarImage} src={avatar} alt="avatar" />
+        </div>
+      </div>
     </header>
   )
 
